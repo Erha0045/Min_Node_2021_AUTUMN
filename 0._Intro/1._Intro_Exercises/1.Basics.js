de4a7f7868052bb033bdfd5438aeeb4f270b24e2
erhan basaran <e.basaran.111@gmail.com>
2021-08-31 19:26:04 +0200

    opgave 1 og noget af opgave 2 lavet

---
+++ 0._Intro/1._Intro_Exercises/1.Basics.js
@@ -0,0 +1,30 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+console.log("my name is: ", firstName, "my lastname is: ", lastName);
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2020";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2021
+// You cannot touch line 1 or 2
+const newYear = Number(year) + number;
+console.log(newYear);
+
+//nedenstående 
+console.log(parseInt("123a23322"));// pasrses as 102 stops before a.
+console.log(Number("102a")); // this is NaN ikke et nummer.
+
+
+// --------------------------------------
