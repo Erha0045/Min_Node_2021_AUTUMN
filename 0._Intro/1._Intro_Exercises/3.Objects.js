02890af4ffa02bb3a880f963d43b032f23465c37
erhan basaran <e.basaran.111@gmail.com>
2021-09-06 21:22:07 +0200

    opgave 3 done

--- 0._Intro/1._Intro_Exercises/3.Objects.js
+++ 0._Intro/1._Intro_Exercises/3.Objects.js
@@ -6,18 +6,18 @@
 const myObj = {"message": "Hello, earthling! I bring peace."};
 
 // Log the message 
-
+    console.log(myObj.message)
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
-
-
+const fName = {"name":"Erhan","age":25};
+console.log(fName)
 // --------------------------------------
 // Exercise 3 - Add a property 
 
-const stackOverflow = {};
-
+const stackOverflow = {age: 25, isAllowed : true};
+console.log(stackOverflow)
 // make a rule called isAllowed and let the value be true
 
 // --------------------------------------
@@ -27,7 +27,9 @@ const thisSong = {"description": "The best song in the world."}
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
-
+delete thisSong.description
+thisSong.about = "Just a tribute."
+console.log(thisSong)
 // --------------------------------------
 
 
